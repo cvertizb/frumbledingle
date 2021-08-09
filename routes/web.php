71c1fbe112c1f89379c1059d1de348f472bb4f37
Carlos Vertiz <carlos@vertiz.net>
2021-08-09 18:55:36 -0500

    Add new routes

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,5 @@ Route::get('/', function () {
 
 Route::get('locations', 'LocationController@index');
 Route::get('items', 'ItemController@index');
+Route::get('categories', 'CategoryController@index');
+Route::get('report', 'ReportController@index');
